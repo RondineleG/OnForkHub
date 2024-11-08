f67a7b37893e1be92f7c78ddda5cfd8b3a11074f
RondineleG <rondineleg@outlook.com>
2024-11-08 10:53:02 -0300

    fix: Change domain exception and add  custom exceptions

---
+++ src/OnForkHub.Core/Exceptions/CustomException.cs
@@ -0,0 +1,6 @@
+namespace OnForkHub.Core.Exceptions;
+
+public abstract class CustomException(string message, string errorCode) : Exception(message)
+{
+    public string ErrorCode { get; } = errorCode;
+}
