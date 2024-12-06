403cae2427ac285037bad8b7ba35c7a287717ae4
Willian dos Santos Almeida Alves <willian81willian@gmail.com>
2024-12-06 17:05:27 -0300

    hotfix(env): remove user mapping

--- .devcontainer/devcontainer_setup.sh
+++ .devcontainer/devcontainer_setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build --build-arg REMOTE_USER=$USER --build-arg REMOTE_UID=$UID --build-arg REMOTE_GID=$UID -t on-fork-hub -f .devcontainer/Dockerfile .
+docker build -t on-fork-hub -f .devcontainer/Dockerfile .
 docker run --rm -it -v $(pwd):/app -w /app on-fork-hub bash
