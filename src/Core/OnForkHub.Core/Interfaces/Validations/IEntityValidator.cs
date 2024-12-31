cc8ba5bedef8af1a1720b0144b397a74c78430e5
Rondinele Guimaraes <rondineleg@outlook.com>
2024-12-31 10:33:10 -0300

    feat: Add usecase to find by id

--- src/Core/OnForkHub.Core/Interfaces/Validations/IEntityValidator.cs
+++ src/Core/OnForkHub.Core/Interfaces/Validations/IEntityValidator.cs
@@ -6,31 +6,3 @@ public interface IEntityValidator<in T>
     IValidationResult Validate(T entity);
     IValidationResult ValidateUpdate(T entity);
 }
-
-public class EntityValidator<T> : IEntityValidator<T>
-    where T : BaseEntity
-{
-    public IValidationResult Validate(T entity)
-    {
-        var result = new ValidationResult();
-
-        if (entity == null)
-        {
-            result.AddError($"{typeof(T).Name} cannot be null.", nameof(entity));
-        }
-
-        return result;
-    }
-
-    public IValidationResult ValidateUpdate(T entity)
-    {
-        var result = Validate(entity);
-
-        if (entity?.Id is null)
-        {
-            result.AddError($"{typeof(T).Name} ID is required for updates.", nameof(entity.Id));
-        }
-
-        return result;
-    }
-}
