ae5054b4b0384ce88336ac808e500623f4d77784
RondineleG <rondineleg@outlook.com>
2025-03-25 16:26:29 -0300

    fix: Refactor API endpoints and update pre-commit script
    - Commented out C# checks and formatting steps in pre-commit script.
    - Removed GetAllEndpoint.cs from API v1.
    - Removed Create.cs, Delete.cs, GetAll.cs, GetById.cs, and Update.cs from API v2.
    - Removed Create.cs and GetAll.cs from API v3.
    - Added GetAllCategoryUseCase.cs in API v3 for RavenDB service.
    - Added Create.cs and GetAll.cs in API v1, previously in API v3.

---
+++ src/Core/OnForkHub.Application/UseCases/Categories/V3/GetAllCategoryUseCase.cs
@@ -0,0 +1,16 @@
+namespace OnForkHub.Application.UseCases.Categories.V3;
+
+public class GetAllCategoryUseCase(ICategoryServiceRavenDB categoryServiceRavenDB) : IUseCase<PaginationRequestDto, IEnumerable<Category>>
+{
+    private readonly ICategoryServiceRavenDB _categoryServiceRavenDB = categoryServiceRavenDB;
+
+    public async Task<RequestResult<IEnumerable<Category>>> ExecuteAsync(PaginationRequestDto request)
+    {
+        ArgumentNullException.ThrowIfNull(request);
+
+        var result = await _categoryServiceRavenDB.GetAllAsync(request.Page, request.ItemsPerPage);
+        return result?.Data is null
+            ? RequestResult<IEnumerable<Category>>.WithNoContent()
+            : RequestResult<IEnumerable<Category>>.Success(result.Data);
+    }
+}
--- src/Presentations/OnForkHub.Api/Endpoints/V1/Categories/GetAll.cs
+++ src/Presentations/OnForkHub.Api/Endpoints/V1/Categories/GetAll.cs
@@ -1,10 +1,10 @@
-namespace OnForkHub.Api.Endpoints.V2.Categories;
+namespace OnForkHub.Api.Endpoints.V1.Categories;
 
 public class GetAll(ILogger<GetAll> logger, IUseCase<PaginationRequestDto, IEnumerable<Category>> useCase) : BaseEndpoint<Category>, IEndpointAsync
 {
-    private const int V2 = 2;
+    private const int V3 = 3;
 
-    private static readonly string Route = GetVersionedRoute(V2);
+    private static readonly string Route = GetVersionedRoute(V3);
 
     private readonly ILogger<GetAll> _logger = logger;
 
@@ -12,25 +12,24 @@ public class GetAll(ILogger<GetAll> logger, IUseCase<PaginationRequestDto, IEnum
 
     public Task<RequestResult> RegisterAsync(WebApplication app)
     {
-        var apiVersionSet = CreateApiVersionSet(app, V2);
+        var apiVersionSet = CreateApiVersionSet(app, V3);
 
         ConfigureEndpoint(
                 app.MapGet(
                     Route,
-                    async (CancellationToken cancellationToken) =>
+                    async ([AsParameters] PaginationRequestDto request, CancellationToken cancellationToken = default) =>
                     {
-                        var request = new PaginationRequestDto { Page = 1, ItemsPerPage = 10 };
                         return await HandleUseCase(_useCase, _logger, request);
                     }
                 )
             )
-            .WithName("GetAllCategoriesV2")
+            .WithName("GetAllCategoriesV3")
             .WithApiVersionSet(apiVersionSet)
-            .MapToApiVersion(V2)
+            .MapToApiVersion(V3)
             .CacheOutput(x => x.Expire(TimeSpan.FromMinutes(10)))
             .WithDescription("Returns all categories")
             .WithSummary("List categories")
-            .WithMetadata(new ApiExplorerSettingsAttribute { GroupName = $"v{V2}" })
+            .WithMetadata(new ApiExplorerSettingsAttribute { GroupName = $"v{V3}" })
             .Produces<RequestResult<IEnumerable<Category>>>();
 
         return Task.FromResult(RequestResult.Success());
