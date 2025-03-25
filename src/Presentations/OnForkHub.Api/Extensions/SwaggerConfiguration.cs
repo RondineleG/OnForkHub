e2ffb95e67b9cb20330529ebe4aa45ff57a0d52a
RondineleG <rondineleg@outlook.com>
2025-03-25 16:43:15 -0300

    feat: Add new category endpoints and update Swagger config
    Introduce Delete, GetById, and Update classes in OnForkHub.Api.Endpoints.V1.Categories namespace to handle category API endpoints. Each class implements IEndpointAsync and extends BaseEndpoint<Category>. Configure endpoints with app.MapPost and metadata. Modify SwaggerConfiguration to remove setup for API versions 2 and 3.

--- src/Presentations/OnForkHub.Api/Extensions/SwaggerConfiguration.cs
+++ src/Presentations/OnForkHub.Api/Extensions/SwaggerConfiguration.cs
@@ -17,26 +17,6 @@ public static class SwaggerConfiguration
                     Description = "API version 1 documentation",
                 }
             );
-
-            options.SwaggerDoc(
-                "v2",
-                new OpenApiInfo
-                {
-                    Version = "v2",
-                    Title = "OnForkHub API - Version 2",
-                    Description = "API version 2 documentation",
-                }
-            );
-
-            options.SwaggerDoc(
-                "v3",
-                new OpenApiInfo
-                {
-                    Version = "v3",
-                    Title = "OnForkHub API - Version 3",
-                    Description = "API version 3 documentation",
-                }
-            );
         });
 
         services
@@ -63,8 +43,6 @@ public static class SwaggerConfiguration
             app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "OnForkHub API V1");
-                c.SwaggerEndpoint("/swagger/v2/swagger.json", "OnForkHub API V3");
-                c.SwaggerEndpoint("/swagger/v3/swagger.json", "OnForkHub API V3");
             });
         }
 
