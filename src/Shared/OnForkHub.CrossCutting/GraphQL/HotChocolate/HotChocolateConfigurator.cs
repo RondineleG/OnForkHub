864451d11206fb1bceb5f0020530ed8166f6733f
RondineleG <rondineleg@outlook.com>
2025-04-21 11:17:09 -0300

    feat: Add HotChocolate integration for GraphQL services
    Introduced a `HotChocolateConfigurator` to dynamically discover
    and register GraphQL queries and mutations. Added supporting
    classes: `HotChocolateEndpoint`, `HotChocolateSchemaBuilder`,
    `HotChocolateMutationBase`, and `HotChocolateQueryBase` to
    streamline schema building and endpoint configuration.
    
    Implemented exception handling for `ReflectionTypeLoadException`
    in `HotChocolateConfigurator`. Ensured validation of descriptor
    types in base classes for queries and mutations.

---
+++ src/Shared/OnForkHub.CrossCutting/GraphQL/HotChocolate/HotChocolateConfigurator.cs
@@ -0,0 +1,47 @@
+using System.Reflection;
+
+namespace OnForkHub.CrossCutting.GraphQL.HotChocolate;
+
+public class HotChocolateConfigurator : IGraphQLConfigurator
+{
+    public void RegisterGraphQLServices(IServiceCollection services)
+    {
+        var requestExecutorBuilder = services.AddGraphQLServer();
+        var hotChocolateSchemaBuilder = new HotChocolateSchemaBuilder(requestExecutorBuilder);
+
+        RegisterOperations(hotChocolateSchemaBuilder);
+
+        hotChocolateSchemaBuilder.Build();
+
+        services.AddSingleton<IGraphQLSchemaBuilder>(hotChocolateSchemaBuilder);
+    }
+
+    private static void RegisterOperations(HotChocolateSchemaBuilder graphQLSchemaBuilder)
+    {
+        var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+
+        foreach (var assembly in assemblies)
+        {
+            try
+            {
+                foreach (var type in assembly.GetTypes()
+                    .Where(t => !t.IsAbstract && typeof(IGraphQLQuery).IsAssignableFrom(t)))
+                {
+                    var query = (IGraphQLQuery)Activator.CreateInstance(type)!;
+                    graphQLSchemaBuilder.AddQuery(query);
+                }
+
+                foreach (var type in assembly.GetTypes()
+                    .Where(t => !t.IsAbstract && typeof(IGraphQLMutation).IsAssignableFrom(t)))
+                {
+                    var mutation = (IGraphQLMutation)Activator.CreateInstance(type)!;
+                    graphQLSchemaBuilder.AddMutation(mutation);
+                }
+            }
+            catch (ReflectionTypeLoadException)
+            {
+                continue;
+            }
+        }
+    }
+}
