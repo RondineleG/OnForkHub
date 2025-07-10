8edc56224713de32f58f533f2dadd44fbee7d5d3
RondineleG <rondineleg@outlook.com>
2025-07-10 02:55:44 -0300

    feat: implement ASP.NET Core Identity with EF Core
    Added new classes and mappings to integrate ASP.NET Core Identity with Entity Framework Core. Updated Directory.Packages.props and OnForkHub.Persistence.csproj to include necessary package references. Created IdentityDataContext for managing identity entities and defined custom mappings for roles, claims, and users. Introduced ApplicationUser class to extend identity functionality. Enhances user authentication and authorization capabilities.

---
+++ src/Infrastructure/OnForkHub.Persistence/Contexts/Mappings/Identity/IdentityRoleClaimMapping.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace OnForkHub.Persistence.Contexts.Mappings.Identity;
+
+public class IdentityRoleClaimMapping : IEntityTypeConfiguration<IdentityRoleClaim<long>>
+{
+    public void Configure(EntityTypeBuilder<IdentityRoleClaim<long>> builder)
+    {
+        builder.ToTable("RoleClaims");
+        builder.HasKey(rc => rc.Id);
+        builder.Property(u => u.ClaimType).HasMaxLength(255);
+        builder.Property(u => u.ClaimValue).HasMaxLength(255);
+    }
+}
---
+++ src/Infrastructure/OnForkHub.Persistence/Contexts/Mappings/Identity/IdentityUserClaimMapping.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace OnForkHub.Persistence.Contexts.Mappings.Identity;
+
+public class IdentityUserClaimMapping : IEntityTypeConfiguration<IdentityUserClaim<long>>
+{
+    public void Configure(EntityTypeBuilder<IdentityUserClaim<long>> builder)
+    {
+        builder.ToTable("UserClaims");
+        builder.HasKey(uc => uc.Id);
+        builder.Property(u => u.ClaimType).HasMaxLength(255);
+        builder.Property(u => u.ClaimValue).HasMaxLength(255);
+    }
+}
