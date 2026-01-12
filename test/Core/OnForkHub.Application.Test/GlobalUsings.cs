a177afb1970216070def6de8a4a236e3af896d45
RondineleG <rondineleg@outlook.com>
2026-01-12 15:41:29 -0300

    test(application): add unit tests for DTOs and use cases
    - Add PagedResultDto tests for pagination logic
    - Add VideoSearchRequestDto and CategorySearchRequestDto tests
    - Add VideoResponseDto and CategoryResponseDto tests
    - Add SearchVideoUseCase and SearchCategoryUseCase tests
    - Add CreateVideoUseCase tests with validation scenarios
    - Update GlobalUsings with IVideoService namespace
    - Add coverage folder to .gitignore

--- test/Core/OnForkHub.Application.Test/GlobalUsings.cs
+++ test/Core/OnForkHub.Application.Test/GlobalUsings.cs
@@ -7,6 +7,7 @@ global using OnForkHub.Application.Services.Base;
 global using OnForkHub.Core.Entities;
 global using OnForkHub.Core.Enums;
 global using OnForkHub.Core.Interfaces.Repositories;
+global using OnForkHub.Core.Interfaces.Services;
 global using OnForkHub.Core.Interfaces.Validations;
 global using OnForkHub.Core.Requests;
 global using OnForkHub.Core.Validations;
